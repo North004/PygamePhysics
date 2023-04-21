477c3c6a36a6cff6be129eae05e0f3aeb399fbc8
Jamie050 <56787922+Jamie050@users.noreply.github.com>
2023-04-21 09:24:35 +0000

    class based

--- main.py
+++ main.py
@@ -5,32 +5,9 @@ import math
 import random
 
 
-#setting variables
-screen_width = 1000
-screen_height = 600
-projectiles = []
-bounce_damping = 0.9
-constant = 0.2
-border = 300
-
-#initializing pygame with innit function
-pygame.init()
-#setting up a display surface with dimensions 400*300
-screen = pygame.display.set_mode((screen_width, screen_height))
-#setting a caption for the window
-pygame.display.set_caption('Simulator')
-
-clock = pygame.time.Clock()
-
-
-#initializing pygame font module
-pygame.font.init()
-#setting up a default font and size
-font = pygame.font.SysFont(None, 24)
-
 class Object:
     #constructor class
-    def __init__(self, position, velocity, acceleration, radius,color):
+    def __init__(self, position, velocity, acceleration, radius, color):
         self.position = position
         self.velocity = velocity
         self.acceleration = acceleration
@@ -50,28 +27,28 @@ class Object:
     #bounce method accepts two parametrs screen width and height
     def bounce(self, screen_width, screen_height):
         #checks over all instances apart from the object which this method belongs to
-        for other in projectiles:
+        for other in Simulator.particles:
             if other != self:
                 #it then calls the collide function with the other object
                 self.collide(other)
         #checks if the sphere is overlapping with border
         if self.position[0] - self.radius < 0:
             # sets the velocity in the other direction and  multiplys by bounce damping
-            self.velocity[0] = abs(self.velocity[0]) * bounce_damping
+            self.velocity[0] = abs(self.velocity[0]) * Simulator.bounce_damping
             #sets position so it is just on the edge and not over the edge
             self.position[0] = self.radius
 
         #repeats 4 times for 4 differant walls varying slightly
         elif self.position[0] + self.radius > screen_width:
-            self.velocity[0] = -abs(self.velocity[0]) * bounce_damping
+            self.velocity[0] = -abs(self.velocity[0]) * Simulator.bounce_damping
             self.position[0] = screen_width - self.radius
 
         if self.position[1] - self.radius < 0:
-            self.velocity[1] = abs(self.velocity[1]) * bounce_damping
+            self.velocity[1] = abs(self.velocity[1]) * Simulator.bounce_damping
             self.position[1] = self.radius
 
         elif self.position[1] + self.radius > screen_height:
-            self.velocity[1] = -abs(self.velocity[1]) * bounce_damping
+            self.velocity[1] = -abs(self.velocity[1]) * Simulator.bounce_damping
             self.position[1] = screen_height - self.radius
 
     def collide(self, other):
@@ -105,113 +82,161 @@ class Object:
                                                               other.radius)
 
             # Update velocities of colliding objects
-            self.velocity[0] = v1f_x * bounce_damping
-            self.velocity[1] = v1f_y* bounce_damping
-            other.velocity[0] = v2f_x* bounce_damping
-            other.velocity[1] = v2f_y* bounce_damping
+            self.velocity[0] = v1f_x 
+            self.velocity[1] = v1f_y 
+            other.velocity[0] = v2f_x
+            other.velocity[1] = v2f_y 
 
 
-##creates an instance of an object from objet class
+class Simulator:
+
+    def __init__(self, acceleration):
+
+        pygame.init()
+        pygame.font.init()
+
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.acceleration = acceleration
+        self.particles = []
+        self.font = pygame.font.SysFont(None, 24)
+        self.screen = pygame.display.set_mode(
+            (self.screen_width, self.screen_height))
+        self.clock = pygame.time.Clock()
+        self.velocity = 10
+        self.angle = 45
+        self.mouse_pos = [200, self.screen_height - 200]
+        self.border = 300
+        self.bounce_damping = 0.8
+        self.constant = 0.1
+
+        #setting a caption for the window
+        pygame.display.set_caption('Simulator')
 
 
+    def drawGui(self):
+               #labels white line
+            text_surface = self.font.render(str(int(self.velocity)), True,
+                                            (255, 255, 255))
+            #displaying text surface to screen
+            self.screen.blit(text_surface,
+                             (self.mouse_pos[0] + 20, self.mouse_pos[1] + 20))
 
-#default data
-velocity = 10
-angle = 45
-mouse_pos = [200,screen_height-200]
+            #labels angle
+            text_surface = self.font.render(str(int(self.angle * (180 / 3.14159))),
+                                            True, (255, 255, 255))
+            self.screen.blit(text_surface, (20, self.screen_height - 20))
 
-#main simulation loop
-while True:
+            #draws a white line resultant component of velocity
+            pygame.draw.line(self.screen, (255, 255, 255),
+                             (0, self.screen_height),
+                             (self.mouse_pos[0], self.mouse_pos[1]), 3)
 
-    clock.tick(60)
-        
-    ## returns a array with mouse x and y position
-    mouse = list(pygame.mouse.get_pos())
-    if  math.sqrt(mouse[0]**2 + (screen_height-mouse[1])**2) < border:
-            mouse_pos = mouse
+            #draws a red line x component of velocity
+            pygame.draw.line(self.screen, (255, 0, 0), (0, self.screen_height),
+                             (self.mouse_pos[0], self.screen_height), 3)
 
-        
-        
-    # validity
-    if mouse_pos[0] != None:
-        #calculating angle
-        if mouse_pos[0] > 0:
-            angle = math.atan((screen_height - mouse_pos[1]) / mouse_pos[0])
-            #calculating velocity
-            velocity =  constant * math.sqrt(((mouse_pos[0])**2 + (screen_height - mouse_pos[1])**2)) 
+            #labels red line with x component of velocity
+            text_surface = self.font.render(
+                str(int(self.velocity * math.cos(self.angle))), True, (255, 0, 0))
+            self.screen.blit(text_surface,
+                             (self.mouse_pos[0] // 2, self.screen_height - 24))
 
+            #draws a blue line y component of velocity
+            pygame.draw.line(self.screen, (0, 0, 255),
+                             (self.mouse_pos[0], self.screen_height),
+                             (self.mouse_pos[0], self.mouse_pos[1]), 3)
+
+            #labels a blue line with y component of velocit
+            text_surface = self.font.render(
+                str(int(self.velocity * math.sin(self.angle))), True, (0, 0, 255))
+            self.screen.blit(text_surface,
+                             (self.mouse_pos[0] + 12, self.screen_height -
+                              (self.screen_height - self.mouse_pos[1]) // 2))
+
+            #draws an arc between circle and projection line
+            pygame.draw.arc(self.screen, (255, 255, 255),
+                            [-50, self.screen_height - 50, 100, 100], 0, self.angle,
+                            3)
+    def run(self):
+        while True:
+
+            self.clock.tick(30)
+
+            ## returns a array with mouse x and y position
+            mouse = list(pygame.mouse.get_pos())
                 
-    #checking for key/button presses
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            #uninitializing pygame
-            pygame.quit()
-            #using system module to close the window
-            sys.exit()
+            if math.sqrt(mouse[0]**2 + (self.screen_height - mouse[1])**2) < self.border:
+                self.mouse_pos = mouse
+                self.angle = math.atan((self.screen_height-self.mouse_pos[1])/self.mouse_pos[0])
+                    
+                
+                                 
+            # validity
+            if self.mouse_pos[0] != None:
+                #calculating angle
+                if self.mouse_pos[0] > 0:
+                    angle = math.atan(
+                        (self.screen_height - self.mouse_pos[1]) /
+                        self.mouse_pos[0])
+                    #calculating velocity
+                    velocity = self.constant * math.sqrt(
+                        ((self.mouse_pos[0])**2 +
+                         (self.screen_height - self.mouse_pos[1])**2))
 
-        #checks for input
-        elif event.type == pygame.MOUSEBUTTONUP and math.sqrt(mouse[0]**2 + (screen_height-mouse[1])**2) < border:
-            #instantiates object and adds it to a list
-            color = (random.randint(50,200),random.randint(50,200),random.randint(50,200))
-            projectiles.append(
-                Object(
-                    [0, screen_height],
-                    [velocity * math.cos(angle), -velocity * math.sin(angle)],
-                    1,
-                    25,
-                    color
-            ))
+            #checking for key/button presses
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    #uninitializing pygame
+                    pygame.quit()
+                    #using system module to close the window
+                    sys.exit()
 
-    #updating display every time the loop repeats
+                #checks for input
+                elif event.type == pygame.MOUSEBUTTONUP and math.sqrt(
+                        mouse[0]**2 +
+                    (self.screen_height - mouse[1])**2) < self.border:
+                    #instantiates object and adds it to a list
+                    color = (random.randint(50, 200), random.randint(50, 200),
+                             random.randint(50, 200))
+                    self.particles.append(
+                        Object([0, self.screen_height//2], [
+                            velocity * math.cos(angle),
+                            -velocity * math.sin(angle)
+                        ], 1, 9, color))
 
-    #updates game state
-    #iterates through all particles in projectiles
-    for particle in projectiles:
-        #applies gravity
-        particle.gravity()
-        #updates position
-        particle.update()
-        particle.bounce(screen_width, screen_height)
+            #updating display every time the loop repeats
 
-    #effectively clears the screen
-    screen.fill((26,26,26))
+            #updates game state
+            #iterates through all particles in projectiles
+            for particle in self.particles:
+                #applies gravity
+                particle.gravity()
+                #updates position
+                particle.update()
+                particle.bounce(self.screen_width, self.screen_height)
 
-    #display touch border
-    pygame.draw.circle(screen, (75,75,75), (0, screen_height),border)
-        
-    #loops over all balls
-    for particle in projectiles:
-        #drawing particle
-        pygame.draw.circle(screen, (particle.color), (particle.position[0], particle.position[1]), particle.radius)
+            #effectively clears the screen
+            self.screen.fill((26, 26, 26))
 
-    #labels white line
-    text_surface = font.render(str(int(velocity)), True, (255, 255, 255))
-    #displaying text surface to screen
-    screen.blit(text_surface, (mouse_pos[0]+20,mouse_pos[1]+20))
+            #display touch border
+            pygame.draw.circle(self.screen, (75, 75, 75),
+                               (0, self.screen_height), self.border)
 
-    #labels angle
-    text_surface = font.render(str(int(angle * (180/3.14159))), True, (255, 255, 255))
-    screen.blit(text_surface, (20,screen_height-20))
+            #loops over all balls
+            for particle in self.particles:
+                #drawing particle
+                pygame.draw.circle(
+                    self.screen, (particle.color),
+                    (particle.position[0], particle.position[1]),
+                    particle.radius)
 
-    #draws a white line resultant component of velocity
-    pygame.draw.line(screen, (255,255,255), (0,screen_height), (mouse_pos[0],mouse_pos[1]),3) 
+            #draws the gui to the screen
+            self.drawGui()
 
-    #draws a red line x component of velocity
-    pygame.draw.line(screen, (255,0,0), (0,screen_height), (mouse_pos[0],screen_height),3) 
+            #passed updates to screen
+            pygame.display.update()
 
-    #labels red line with x component of velocity
-    text_surface = font.render(str(int(velocity * math.cos(angle))), True, (255,0,0))
-    screen.blit(text_surface, (mouse_pos[0]//2,screen_height-24))
 
-    #draws a blue line y component of velocity
-    pygame.draw.line(screen, (0,0,255), (mouse_pos[0],screen_height), (mouse_pos[0],mouse_pos[1]),3) 
-
-    #labels a blue line with y component of velocit
-    text_surface = font.render(str(int(velocity * math.sin(angle))), True, (0,0,255))
-    screen.blit(text_surface, (mouse_pos[0]+12,screen_height - (screen_height-mouse_pos[1])//2 ))
-        
-    #draws an arc between circle and projection line
-    pygame.draw.arc(screen, (255,255,255),[-50,screen_height-50,100,100], 0,angle,3)
-        
-    #passed updates to screen
-    pygame.display.update()
+Simulator = Simulator(1)
+Simulator.run()
